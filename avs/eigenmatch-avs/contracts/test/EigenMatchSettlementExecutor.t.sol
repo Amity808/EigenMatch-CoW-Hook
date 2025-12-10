f989e3d53e32d93e71221faf7f83a3d17e7547d2
amity-76 <ikpiablessingisioma@gmail.com>
2025-12-10 01:34:26 +0100

    Add executor watcher signature tests

--- avs/eigenmatch-avs/contracts/test/EigenMatchSettlementExecutor.t.sol
+++ avs/eigenmatch-avs/contracts/test/EigenMatchSettlementExecutor.t.sol
@@ -15,6 +15,8 @@ contract EigenMatchSettlementExecutorTest is Test {
 
     address private watcher;
     uint256 private watcherKey;
+    address private watcherTwo;
+    uint256 private watcherTwoKey;
 
     bytes32 private constant MEASUREMENT = keccak256("measurement");
     bytes32 private constant DIGEST = keccak256("digest");
@@ -26,16 +28,18 @@ contract EigenMatchSettlementExecutorTest is Test {
 
         hook = new EigenMatchHookMock();
         (watcher, watcherKey) = makeAddrAndKey("watcher");
+        (watcherTwo, watcherTwoKey) = makeAddrAndKey("watcher-two");
 
-        address[] memory watchers = new address[](1);
+        address[] memory watchers = new address[](2);
         watchers[0] = watcher;
+        watchers[1] = watcherTwo;
 
         executor = new EigenMatchSettlementExecutor(hook, registry, address(this), watchers, 1);
     }
 
     function testSubmitSettlementForwardsToHook() public {
         EigenMatchSettlementExecutor.BundleSubmission memory submission = _buildSubmission();
-        submission.watcherSignatures = _sign(submission.bundle.bundleId, submission.bundle.replaySalt);
+        submission.watcherSignatures = _signDefault(submission.bundle.bundleId, submission.bundle.replaySalt);
 
         executor.submitSettlement(submission);
 
@@ -46,7 +50,7 @@ contract EigenMatchSettlementExecutorTest is Test {
     function testSubmitSettlementRevertsWhenDigestRevoked() public {
         EigenMatchSettlementExecutor.BundleSubmission memory submission = _buildSubmission();
         registry.setDigestRevoked(DIGEST, true);
-        submission.watcherSignatures = _sign(submission.bundle.bundleId, submission.bundle.replaySalt);
+        submission.watcherSignatures = _signDefault(submission.bundle.bundleId, submission.bundle.replaySalt);
 
         vm.expectRevert(
             abi.encodeWithSelector(
@@ -62,6 +66,50 @@ contract EigenMatchSettlementExecutorTest is Test {
 
         vm.expectRevert(
             abi.encodeWithSelector(EigenMatchSettlementExecutor.InsufficientWatcherSigners.selector, 1, 0)
+        );
+        executor.submitSettlement(submission);
+    }
+
+    function testSubmitSettlementRejectsDuplicateWatcherSignatures() public {
+        EigenMatchSettlementExecutor local = new EigenMatchSettlementExecutor(
+            hook, registry, address(this), _twoWatchers(), 2
+        );
+        EigenMatchSettlementExecutor.BundleSubmission memory submission = _buildSubmission();
+
+        bytes32 digest = local.watcherMessageHash(submission.bundle.bundleId, submission.bundle.replaySalt);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(watcherKey, digest);
+
+        submission.watcherSignatures = new bytes[](2);
+        submission.watcherSignatures[0] = abi.encodePacked(r, s, v);
+        submission.watcherSignatures[1] = abi.encodePacked(r, s, v);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(EigenMatchSettlementExecutor.DuplicateWatcherSignature.selector, watcher)
+        );
+        local.submitSettlement(submission);
+    }
+
+    function testSubmitSettlementRejectsUnknownWatcherSignature() public {
+        EigenMatchSettlementExecutor.BundleSubmission memory submission = _buildSubmission();
+        bytes32 digest = executor.watcherMessageHash(submission.bundle.bundleId, submission.bundle.replaySalt);
+        (address stranger, uint256 strangerKey) = makeAddrAndKey("stranger");
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(strangerKey, digest);
+        submission.watcherSignatures = new bytes[](1);
+        submission.watcherSignatures[0] = abi.encodePacked(r, s, v);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(EigenMatchSettlementExecutor.InvalidWatcherSignature.selector, stranger)
+        );
+        executor.submitSettlement(submission);
+    }
+
+    function testSubmitSettlementRejectsDuplicateBundles() public {
+        EigenMatchSettlementExecutor.BundleSubmission memory submission = _buildSubmission();
+        submission.watcherSignatures = _signDefault(submission.bundle.bundleId, submission.bundle.replaySalt);
+        executor.submitSettlement(submission);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(EigenMatchSettlementExecutor.BundleAlreadyProcessed.selector, submission.bundle.bundleId)
         );
         executor.submitSettlement(submission);
     }
@@ -97,13 +145,16 @@ contract EigenMatchSettlementExecutorTest is Test {
         submission.traderSettlements = settlements;
     }
 
-    function _sign(
-        bytes32 bundleId,
-        bytes32 replaySalt
-    ) private view returns (bytes[] memory sigs) {
+    function _signDefault(bytes32 bundleId, bytes32 replaySalt) private view returns (bytes[] memory sigs) {
         bytes32 digest = executor.watcherMessageHash(bundleId, replaySalt);
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(watcherKey, digest);
         sigs = new bytes[](1);
         sigs[0] = abi.encodePacked(r, s, v);
+    }
+
+    function _twoWatchers() private view returns (address[] memory watchers) {
+        watchers = new address[](2);
+        watchers[0] = watcher;
+        watchers[1] = watcherTwo;
     }
 }
